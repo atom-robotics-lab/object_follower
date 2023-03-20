388d11536cacedec4ab6dc94e33cadfb48a963a8
Jasmeet Singh <jasmeet0915@gmail.com>
2023-03-20 18:33:17 +0530

    Added sim launch file

---
+++ object_follower_sim/launch/gz_sim.launch.py
@@ -0,0 +1,24 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
+
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
+                )           
+            )
+
+    return LaunchDescription([
+        gazebo
+    ])
