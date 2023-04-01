073ce8688c809ffe96a735c691045bd19a75861e
Jasmeet Singh <jasmeet0915@gmail.com>
2023-04-01 11:09:07 +0530

    Added robot launch

--- object_follower_sim/launch/mr_robot.launch.py
+++ object_follower_sim/launch/mr_robot.launch.py
@@ -10,8 +10,11 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     pkg_object_follower_sim = get_package_share_directory("object_follower_sim")
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     mr_robot_xacro = os.path.join(pkg_object_follower_sim, "urdf", "mr_robot.xacro")
     assert os.path.exists(mr_robot_xacro), "No robot found in {}".format(mr_robot_xacro)
@@ -19,14 +22,29 @@ def generate_launch_description():
     mr_robot_desc_config = xacro.process_file(mr_robot_xacro)
     mr_robot_urdf = mr_robot_desc_config.toxml()
 
+    mr_robot_sdf = os.path.join(pkg_object_follower_sim, "urdf", "mr_robot.sdf")
+    urdf_path = os.path.join(pkg_object_follower_sim, "urdf", "mr_robot.urdf")
+
     spawn_robot = Node(package="ros_gz_sim", executable="create",
                     arguments=["-name", "mr_robot",
                                 "-x", "0.0",
-                                "-y", "0.0",
+                                "-y", "3.0",
                                 "-z", "0.0",
                                 "-Y", "1.57",
-                                "-string", mr_robot_urdf])
+                                "-file", mr_robot_sdf])
+
+    # robot state publisher node
+    state_publisher = Node(package='robot_state_publisher', executable='robot_state_publisher',
+				output='screen',
+				parameters = [
+					{'ignore_timestamp': False},
+                    {'use_sim_time': use_sim_time},
+					{'use_tf_static': True},
+					{'robot_description': open(urdf_path).read()}],
+				arguments = [urdf_path])
 
     return LaunchDescription([
-        spawn_robot
+        DeclareLaunchArgument('use_sim_time', default_value="true"),
+        spawn_robot,
+        state_publisher
     ])
